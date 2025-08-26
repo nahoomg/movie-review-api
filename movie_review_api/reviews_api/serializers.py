777a9118efcd95df173f8329b9725b1d4edcd6ad
nahoomg <nahomgetachewmy@gmail.com>
2025-08-26 12:13:03 +0300

    Add UserCreateSerializer for user registration with password handling

--- movie_review_api/reviews_api/serializers.py
+++ movie_review_api/reviews_api/serializers.py
@@ -24,3 +24,18 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
